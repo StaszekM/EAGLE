92319c143e5508eaaebf501cda42a86631f0fbb4
Stanisław Markowski <stanislaw.markowski@pwr.edu.pl>
2025-10-15 14:50:35 +0200

    Add missing 120k length limit

--- eagle/evaluation/gen_baseline_answer_llama3chat.py
+++ eagle/evaluation/gen_baseline_answer_llama3chat.py
@@ -248,6 +248,7 @@ def get_model_answers(
                 log=True,
                 is_llama3=True,
                 max_new_tokens=120_000,
+                max_length=120_000,
             )
             torch.cuda.synchronize()
             total_time = time.time() - start_time
